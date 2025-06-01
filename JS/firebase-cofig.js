e8af0dcd8632ee8c20f12cf42333e71c5dba13b1
quocviet1234890 <vietnguyen1234890@gmail.com>
2025-06-01 11:11:38 +0700

    inti repo

--- JS/firebase-cofig.js
+++ JS/firebase-cofig.js
@@ -1,4 +1,6 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyD2MSTcDGHiom_SdRgjQuAkoLmeWCtjywg",
     authDomain: "mindx-a8b1a.firebaseapp.com",
@@ -7,7 +9,8 @@ const firebaseConfig = {
     messagingSenderId: "374588426457",
     appId: "1:374588426457:web:7693a5f3fb3fca8f5116c8",
     measurementId: "G-H0H95FFWWF"
-  };
+};
 
-  
-  
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { auth };
--- JS/login.js
+++ JS/login.js
@@ -1,1 +1,30 @@
-    
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { auth } from "./firebase-config.js";
+
+const form = document.getElementById("login-form");
+const errorNotification = document.getElementById("login-error");
+const successNotification = document.getElementById("login-notification");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("login-username").value;
+    const password = document.getElementById("login-password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("Đăng nhập thành công:", user.email);
+
+            successNotification.style.display = "block";
+            errorNotification.style.display = "none";
+
+            window.location.href = "index.html";
+        })
+        .catch((error) => {
+            console.error("Lỗi đăng nhập:", error.message);
+
+            successNotification.style.display = "none";
+            errorNotification.style.display = "block";
+        });
+});
