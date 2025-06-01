852306389c2c81ef1349b930c2d7018908c4eccc
quocviet1234890 <vietnguyen1234890@gmail.com>
2025-06-01 10:09:59 +0700

    init repo

---
+++ JS/firebase-cofig.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD2MSTcDGHiom_SdRgjQuAkoLmeWCtjywg",
+    authDomain: "mindx-a8b1a.firebaseapp.com",
+    projectId: "mindx-a8b1a",
+    storageBucket: "mindx-a8b1a.firebasestorage.app",
+    messagingSenderId: "374588426457",
+    appId: "1:374588426457:web:7693a5f3fb3fca8f5116c8",
+    measurementId: "G-H0H95FFWWF"
+  };
+
+  
+  
