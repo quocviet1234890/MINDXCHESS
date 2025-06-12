f5996be1d47b5d56b22423dc7c18bf1785827b6f
quocviet1234890 <vietnguyen1234890@gmail.com>
2025-06-12 22:17:06 +0700

    init repo

--- JS/firebase-cofig.js
+++ JS/firebase-cofig.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyD2MSTcDGHiom_SdRgjQuAkoLmeWCtjywg",
-    authDomain: "mindx-a8b1a.firebaseapp.com",
-    projectId: "mindx-a8b1a",
-    storageBucket: "mindx-a8b1a.firebasestorage.app",
-    messagingSenderId: "374588426457",
-    appId: "1:374588426457:web:7693a5f3fb3fca8f5116c8",
-    measurementId: "G-H0H95FFWWF"
+  apiKey: "AIzaSyD2MSTcDGHiom_SdRgjQuAkoLmeWCtjywg",
+  authDomain: "mindx-a8b1a.firebaseapp.com",
+  projectId: "mindx-a8b1a",
+  storageBucket: "mindx-a8b1a.firebasestorage.app",
+  messagingSenderId: "374588426457",
+  appId: "1:374588426457:web:7693a5f3fb3fca8f5116c8",
+  measurementId: "G-H0H95FFWWF"
 };
 
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
